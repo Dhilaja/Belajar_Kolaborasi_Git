105ad1f939b643a9037e1d91b5035154c074c96b
Dhilaja <fadhil@smkitihsanulfikri.sch.id>
2026-01-27 11:54:04 +0700

    Update biodata_fadhil.py

--- biodata_fadhil.py
+++ biodata_fadhil.py
@@ -3,7 +3,7 @@ print("Program Biodata Sederhana Dengan Python")
 print("======================================\n")
 
 nama    = "Fadhil"                          # Ganti kalau nama lengkap beda
-hobi    = "Coding, main game, eksplor GitHub"  # Isi hobi kamu
+hobi    = "ngoding, main game, eksplor GitHub"  # Isi hobi kamu
 alamat  = "Jalan [isi alamat kamu di Yogyakarta]"  # Isi alamat lengkap
 kota    = "Yogyakarta"                      # Isi kota
 
